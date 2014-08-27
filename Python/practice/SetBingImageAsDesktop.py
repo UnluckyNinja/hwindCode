0e422661dda2c08bdff216fd8e75764d14e3b7d8
unknown <guohao@hwind-work.redmond.corp.microsoft.com>
2014-08-27 16:50:01 -0700

    This script is used to: 1. get today's bing image (with high resolution) 2. set it as desktop

---
+++ Python/practice/SetBingImageAsDesktop.py
@@ -0,0 +1,71 @@
+'''
+Created on Aug 27, 2014
+
+@author: hwind
+'''
+
+import json
+import urllib.request
+import urllib.parse
+import os
+import ctypes
+import pythoncom
+from win32com.shell import shell, shellcon
+from datetime import date, datetime
+from pathlib import Path
+
+def GetCharsetFromResponse (response):
+    contentType = response.getheader("content-type")
+    for item in contentType.split(' '):
+        if item.lower().startswith("charset"):
+            return item[item.index("="):]
+
+
+def LogWarning (msg):
+    print("%s    %s", datetime.today().isoformat(), msg)
+
+def DownloadImage(url, dest):
+    if os.path.exists(dest):
+        print("path already exists")
+        return
+    fp = open(dest, "wb")
+    response = urllib.request.urlopen(url)
+    fp.write(response.read())
+    fp.close()
+    response.close()
+    
+"""
+This version doesn't work on Windows8
+def UpdateDesktopBackground(dest):
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, dest, 3)
+"""
+def UpdateDesktopBackground(dest):
+    iad = pythoncom.CoCreateInstance(shell.CLSID_ActiveDesktop, None,
+                                     pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IActiveDesktop)
+    iad.SetWallpaper(dest, 0)
+    iad.ApplyChanges(shellcon.AD_APPLY_ALL)
+    
+
+bingImageInfoURL = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+baseFolder = "D:\documents\OneDrive\图片\wallpaper"
+response = urllib.request.urlopen(bingImageInfoURL)
+charset = GetCharsetFromResponse(response)
+content = response.read().decode(charset)
+response.close()
+
+imageInfo = json.loads(content)
+for img in imageInfo["images"]:
+    if img["startdate"] != date.today().strftime("%Y%m%d"):
+        LogWarning("Didn't find today's image info")
+    else:
+        imgHighResolution = img["urlbase"]+"_1920x1200.jpg"
+        imageUrl = urllib.parse.urljoin("http://www.bing.com", imgHighResolution)
+        path, imgName = os.path.split(imgHighResolution)
+        filename = date.today().strftime("%Y%m%d")+"_"+imgName            
+        dest = os.path.join(baseFolder, filename)
+        DownloadImage(imageUrl, dest)
+        UpdateDesktopBackground(dest)
+        break
+
+
