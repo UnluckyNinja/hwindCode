b567ae9d70c346b8391c09eb238d7aac4c8c056d
Guoyu Hao <haoguoyu@gmail.com>
2015-06-12 16:04:26 -0700

    Add OAuth2 support for google account

--- Python/pyvideo_project/pyvideo/urls.py
+++ Python/pyvideo_project/pyvideo/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^$', home, name='home'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^accounts/', include('allauth.urls')),
 ]
