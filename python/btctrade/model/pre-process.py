08dd28beb443583833acad1fab16f9a260d8c856
hwind <haoguoyu@gmail.com>
2015-11-22 23:00:56 -0800

    use spark python to pre process the data. This is a replacement of the .hql hive solution

---
+++ python/btctrade/model/pre-process.py
@@ -0,0 +1,58 @@
+import sys
+import uuid
+from datetime import datetime
+from datetime import timedelta
+from pyspark import SparkContext, SparkConf
+
+def _get_task_partitions(start, end=None):
+    format_str = "%Y-%m-%d/%H"
+    start_time = datetime.strptime(start, format_str) 
+    if (end == None):
+        end_time = datetime.utcnow()
+    else:
+        end_time = datetime.strptime(end, format_str)
+
+    partitions = []
+    cur_time = start_time
+    while cur_time <= end_time:
+        cur_str = cur_time.strftime(format_str)
+        partitions.append(cur_str)
+        cur_time = cur_time + timedelta(hours=1)
+
+    return partitions
+
+
+def pre_process_logs(start="2015-11-17/07", end=None):
+    task_partitions = _get_task_partitions(start, end)
+    print(task_partitions)
+
+    conf = SparkConf()
+    conf.setAppName("pre-process")
+    sc = SparkContext(conf = conf)
+    url_template = "wasb://btcc@guohaodata.blob.core.windows.net/btccny-price-and-order/{0}/*"
+    union_src = sc.textFile(url_template.format(task_partitions[0]))
+    for i in range(1, len(task_partitions)):
+        cur_src = sc.textFile(url_template.format(task_partitions[i]))
+        union_src = union_src.union(cur_src)
+
+    #src = sc.textFile("wasb://btcc@guohaodata.blob.core.windows.net/btccny-price-and-order/2*/*/*")
+
+    items = union_src.map(lambda x: x.split("\t")).map(lambda x: (long(x[0]), (long(x[0]), float(x[3]), float(x[4]), float(x[5])))).filter(lambda x: x[0] >= 1447745420).reduceByKey(lambda x, y : x)
+
+    sorted_items = items.repartition(1).sortByKey().zipWithIndex().map(lambda x: "\t".join((str(x[1]), str(x[0][1][0]), str(x[0][1][1]), str(x[0][1][2]), str(x[0][1][3]))))
+
+    path = "wasb://btcc@guohaodata.blob.core.windows.net/cooked/" + str(uuid.uuid4())
+    print("results will be saved under {0}".format(path))
+    sorted_items.saveAsTextFile(path)
+    return path
+
+if (__name__ == "__main__"):
+    start = None
+    end = None
+    if (len(sys.argv) == 2):
+        start = sys.argv[1]
+    else:
+        start = sys.argv[1]
+        end = sys.argv[2]
+
+    pre_process_logs(start, end)
