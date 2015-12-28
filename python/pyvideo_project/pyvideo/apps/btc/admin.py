bdb175378c1336d6fd53ff640a3befd3c4e90b27
hwind <haoguoyu@gmail.com>
2015-12-28 11:07:05 -0800

    Add a field

--- python/pyvideo_project/pyvideo/apps/btc/admin.py
+++ python/pyvideo_project/pyvideo/apps/btc/admin.py
@@ -4,7 +4,7 @@ from . import models
 
 @admin.register(models.BTCTrade)
 class BTCTradeAdmin(admin.ModelAdmin):
-    list_display = ['timestamp', 'btcc_btc_free', 'btcc_btc_frozen', 'btcc_cny_free', 'btcc_cny_frozen'
+    list_display = ['timestamp', 'src', 'btcc_btc_free', 'btcc_btc_frozen', 'btcc_cny_free', 'btcc_cny_frozen'
         ,'btcc_price', 'ok_btc_free', 'ok_btc_frozen', 'ok_cny_free', 'ok_cny_frozen', 'ok_price']
 
     search_fields = ['timestamp']
